43864d42a579eb857fdb448ef3e70c96d889c5cd
Anderson Lemke <amatheuslemke@hotmail.com>
2023-01-30 21:49:46 -0300

    feat: config button style

--- dw9_delivery_app/lib/app/pages/splash/splash_page.dart
+++ dw9_delivery_app/lib/app/pages/splash/splash_page.dart
@@ -1,3 +1,6 @@
+import 'package:dw9_delivery_app/app/core/ui/styles/app_styles.dart';
+import 'package:dw9_delivery_app/app/core/ui/styles/colors_app.dart';
+import 'package:dw9_delivery_app/app/core/ui/widgets/delivery_button.dart';
 import 'package:flutter/material.dart';
 
 class SplashPage extends StatelessWidget {
@@ -9,7 +12,18 @@ class SplashPage extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Splash'),
       ),
-      body: Container(),
+      body: Column(children: [
+        Container(),
+        DeliveryButton(
+          label: 'Label',
+          onPressed: () {},
+          width: 200,
+          height: 50,
+        ),
+        TextFormField(
+          decoration: InputDecoration(labelText: 'text'),
+        )
+      ]),
     );
   }
 }
